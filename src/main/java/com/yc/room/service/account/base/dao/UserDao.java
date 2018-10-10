2bf883bc83cdf0485823c14c53579dfccf4edac5
lucky <xwluckystar@163.com>
2018-10-10 15:10:05 +0800

    账户登录项目初始化

---
+++ src/main/java/com/yc/room/service/account/base/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.yc.room.service.account.base.dao;
+
+
+import com.yc.room.service.roomErp.webgis.base.model.BaseUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * userDao
+ */
+
+public interface UserDao extends JpaRepository<BaseUser, Integer> {
+
+    List<BaseUser> findAll();
+
+    BaseUser findBaseUserByName(String name);
+
+}
---
+++ src/main/java/com/yc/room/service/account/base/service/LoginService.java
@@ -0,0 +1,13 @@
+package com.yc.room.service.account.base.service;
+
+/**
+ * @program: roomErp
+ * @description: 登录接口
+ * @author: xing.yang
+ * @create: 2018-09-18 17:24
+ **/
+
+public interface LoginService {
+
+
+}
